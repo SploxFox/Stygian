4b8a7f10a253dcb37f267657ee37afc7ba9b0942
SploxFox <sploxfox@gmail.com>
2019-08-20 16:25:48 -0500

    Basic example up

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import {Game} from "../../Iris/src";
+import { TextBox } from "../../Iris/src/interface/text-box";
+import { Localizer } from "../../Iris/src/localizer";
+import { FormattedText } from "../../Iris/src/interface/formatted-text";
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+import { MainTextBox } from "./interface/main-text-box";
+
+const game = new Game();
+
+game.localizer.loadAsset("story/sampleStoryInteraction.txt").then((text) => {
+    const formattedText = FormattedText.parse(text);
+
+    let mainTextBox: MainTextBox;
+    ReactDOM.render(<MainTextBox formattedText={formattedText} ref={(mtb) => mainTextBox = mtb}></MainTextBox>,document.body);
+
+    window.addEventListener("click", function(){
+        mainTextBox.textBox.start();
+    });
+});
---
+++ src/interface/continue-arrow.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export class ContinueArrow extends React.Component<{}> {
+    render() {
+        return(
+            <img className="continue-arrow" src="assets/universal/icons/continue-arrow.svg">
+            
+            </img>
+        )
+    }
+}
