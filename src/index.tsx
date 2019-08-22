748123b82f13381e3b03a8862e909d1c2355da78
SploxFox <sploxfox@gmail.com>
2019-08-22 15:32:08 -0500

    Renamed main text box to interactive text box; moved into Iris; added some comments

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
 import {Game} from "../../Iris/src";
-import { TextBox } from "../../Iris/src/interface/text-box";
-import { Localizer } from "../../Iris/src/localizer";
 import { FormattedText } from "../../Iris/src/interface/formatted-text";
 import * as ReactDOM from "react-dom";
 import * as React from "react";
-import { MainTextBox } from "./interface/main-text-box";
+import { InteractiveTextBox } from "../../Iris/src/interface/text-box/interactive-text-box";
 
+//Create a new instance of the game class
 const game = new Game();
 
+//Load the story text document...
 game.localizer.loadAsset("story/sampleStoryInteraction.txt").then((text) => {
+
+    //...and, once we've loaded that, we'll execute all of this code
+
+    //Parses the plain text into FormattedText (which is how we get the dancing letters and such)
     const formattedText = FormattedText.parse(text);
 
-    let mainTextBox: MainTextBox;
-    ReactDOM.render(<MainTextBox formattedText={formattedText} ref={(mtb) => mainTextBox = mtb}></MainTextBox>,document.body);
+    //Create a new variable with type InteractiveTextbox (but do not assign anything yet)
+    let mainTextBox: InteractiveTextBox;
 
+    //Render a new Interactive text box                                Once mounted onto the page, we'll set that mainTextBox variable to our new text box
+    ReactDOM.render(<InteractiveTextBox formattedText={formattedText} ref={(mtb: any) => mainTextBox = mtb}></InteractiveTextBox>,document.body);
+
+    //When we click or touch, we'll start the text box
     window.addEventListener("click", function(){
         mainTextBox.textBox.start();
     });
